978d53f3df5bab310bc69ca1eb077fd994543f31
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-10-12 19:58:10 +0300

    CSS
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/core/components/image/UploadGrid.tsx
+++ src/core/components/image/UploadGrid.tsx
@@ -8,22 +8,26 @@ export default function UploadGrid() {
 
   return (
     <>
-      <input
-        multiple
-        onChange={(ev) => {
-          ev.persist()
-          let _files: File[] = []
-          if (ev.target.files) {
-            _files = Array.from(ev.target.files)
-          }
-          _files = _files.slice(0, +max - totalCount)
-          setFiles([...files, ..._files])
-          ev.target.value = ""
-        }}
-        accept="image/*"
-        id="icon-button-file"
-        type="file"
-      />
+      <label className="btn btn-primary">
+        <i className="fa fa-image">[Icon]</i> {"Adaugă fotografii"}
+        <input
+          className="hidden"
+          multiple
+          onChange={(ev) => {
+            ev.persist()
+            let _files: File[] = []
+            if (ev.target.files) {
+              _files = Array.from(ev.target.files)
+            }
+            _files = _files.slice(0, +max - totalCount)
+            setFiles([...files, ..._files])
+            ev.target.value = ""
+          }}
+          accept="image/*"
+          id="icon-button-file"
+          type="file"
+        />
+      </label>
       <div className={"grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3"}>
         {files.map((file, idx) => (
           <ImageUpload key={idx} file={file} />
