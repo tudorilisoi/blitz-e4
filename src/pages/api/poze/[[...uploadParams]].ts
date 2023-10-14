7ced0b6748f697ca1cf822c40506150691a7542b
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-10-14 21:51:37 +0300

    serve an image when the actual one is not found
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/pages/api/poze/[[...uploadParams]].ts
+++ src/pages/api/poze/[[...uploadParams]].ts
@@ -1,8 +1,7 @@
-import { NotFoundError } from "blitz"
 import db from "db"
 import fs from "fs"
-import mime from "mime-types"
 import { ServerResponse } from "http"
+import mime from "mime-types"
 import { UPLOADS_PATH } from "src/config"
 const fsp = fs.promises
 // NOTE Only assets that are in the public directory at build time will be served by Next.js
@@ -11,15 +10,26 @@ export default async function serveUpload(req, res: ServerResponse) {
   console.log(req.query)
   const { uploadParams } = req.query
   const id = parseInt(uploadParams[0])
-  const image = await db.image.findFirst({ where: { id } })
-  if (!image) {
-    throw new NotFoundError("No such record")
+  try {
+    // TODO make a serveImage utility function
+    const image = await db.image.findFirst({ where: { id } })
+    const path = `${UPLOADS_PATH}/${image?.fileName}`
+    const blob = await fsp.readFile(path)
+    const mimeType = mime.lookup(path)
+    res.setHeader("Content-Type", mimeType)
+    res.statusCode = 200
+    res.write(blob)
+    res.end()
+  } catch (error) {
+    // throw new NotFoundError("No such record")
+
+    const notFoundPath = `${process.cwd()}/public/404.webp`
+    const blob = await fsp.readFile(notFoundPath)
+    const mimeType = mime.lookup(notFoundPath)
+    res.setHeader("Content-Type", mimeType)
+    res.statusCode = 404
+    res.write(blob)
+    res.end()
+    return
   }
-  const path = `${UPLOADS_PATH}/${image.fileName}`
-  const blob = await fsp.readFile(path)
-  const mimeType = mime.lookup(path)
-  res.setHeader("Content-Type", mimeType)
-  res.statusCode = 200
-  res.write(blob)
-  res.end()
 }
