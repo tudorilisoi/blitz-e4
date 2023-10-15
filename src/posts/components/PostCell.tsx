083e36c68e35b77803c6b80cd753176b30da9f35
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-10-15 16:25:24 +0300

    WIP
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/posts/components/PostCell.tsx
+++ src/posts/components/PostCell.tsx
@@ -1,8 +1,9 @@
-import { Category, Post } from "@prisma/client"
+import { Category, Post, Image } from "@prisma/client"
 import Link from "next/link"
 import { shortenText } from "src/helpers"
 import { makePostNavUrl } from "src/pages/anunt/[[...params]]"
-const PostCell = ({ post, category }: { post: Post; category: Category }) => {
+const PostCell = ({ post, category }: { post: Post & { images: Image[] }; category: Category }) => {
+  const { images } = post
   return (
     <div key={post.id} className="bg-white p-4 rounded-lg shadow-md">
       <h2 className="text-xl font-semibold mb-2 text-blue-800">
@@ -10,6 +11,9 @@ const PostCell = ({ post, category }: { post: Post; category: Category }) => {
           {shortenText(post.title, 100)}
         </Link>
       </h2>
+      {images.length > 0 ? (
+        <span className="text-sm text-blue-500">{post.images.length} fotografii</span>
+      ) : null}
       <p className="text-gray-600">{shortenText(post.body, 140)}</p>
     </div>
   )
