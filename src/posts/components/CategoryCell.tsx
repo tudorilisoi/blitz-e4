768ba49828ca7c4e24454280c4fa71a2fab19cd7
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-10-18 13:23:53 +0300

    WIP categories on the home page
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/posts/components/CategoryCell.tsx
+++ src/posts/components/CategoryCell.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link"
-import { shortenText } from "src/helpers"
+import { pluralize, shortenText } from "src/helpers"
 import { makePostsNavUrl } from "src/pages/anunturi/[[...params]]"
 import { CategoryWithCounters } from "../helpers"
 const CategoryCell = ({ category }: { category: CategoryWithCounters }) => {
+  const { postCount } = category
   return (
     <div key={category.id} className="p-4 rounded-md  bg-primary bg-opacity-20  text-base-content ">
       <h2 className="text-xl font-semibold mb-2 text-accent hover:text-accent-focus hover:underline">
@@ -11,7 +12,13 @@ const CategoryCell = ({ category }: { category: CategoryWithCounters }) => {
         </Link>
       </h2>
 
-      <span className="text-sm font-extrabold ">{category.postCount} anunţuri</span>
+      <span className="text-sm font-extrabold ">
+        {pluralize(postCount, {
+          none: "niciun anunţ",
+          one: "1 anunţ",
+          many: `${postCount} anunţuri`,
+        })}
+      </span>
 
       <p className="">{shortenText(category.description, 140)}</p>
     </div>
