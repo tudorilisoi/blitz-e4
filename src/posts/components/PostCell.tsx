0dba1ef0a8b2ca5e42bdb49cf6182a6925f46760
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-10-18 14:01:22 +0300

    WIP
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/posts/components/PostCell.tsx
+++ src/posts/components/PostCell.tsx
@@ -5,14 +5,17 @@ import { PostWithIncludes } from "../helpers"
 const PostCell = ({ post }: { post: PostWithIncludes }) => {
   const { images } = post
   return (
-    <div key={post.id} className="p-4 rounded-md  bg-primary bg-opacity-20  text-base-content ">
-      <h2 className="text-xl font-semibold mb-2 text-accent hover:text-accent-focus hover:underline">
-        <Link href={makePostNavUrl(post)}>{shortenText(post.title, 100)}</Link>
-      </h2>
-      {images.length > 0 ? (
-        <span className="text-sm font-extrabold ">{post.images.length} fotografii</span>
-      ) : null}
-      <p className="">{shortenText(post.body, 140)}</p>
+    <div className="flex flex-row flex-nowrap p-4 rounded-md  bg-primary bg-opacity-20 ">
+      <div className="  text-base-content flex-shrink flex-1 ">
+        <h2 className="text-xl font-semibold mb-2 text-accent hover:text-accent-focus hover:underline">
+          <Link href={makePostNavUrl(post)}>{shortenText(post.title, 100)}</Link>
+        </h2>
+        {images.length > 0 ? (
+          <span className="text-sm font-extrabold ">{post.images.length} fotografii</span>
+        ) : null}
+        <p className="">{shortenText(post.body, 140)}</p>
+      </div>
+      <div className="">Image here!!!</div>
     </div>
   )
 }
