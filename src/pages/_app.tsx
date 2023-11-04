22497a878cc67e1b964c8ec8fc5b7c54eb165cce
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-04 23:35:39 +0200

    better errors
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,30 @@
 import { AppProps, ErrorBoundary, ErrorComponent, ErrorFallbackProps } from "@blitzjs/next"
 import { AuthenticationError, AuthorizationError } from "blitz"
 import { withBlitz } from "src/blitz-client"
+import ViewportCentered from "src/core/components/spinner/ViewPortCentered"
 import "src/styles/init.css"
 import { trpc } from "src/ws-utils/trpc"
 
 function RootErrorFallback({ error }: ErrorFallbackProps) {
+  let returnValue: JSX.Element | null = null
   if (error instanceof AuthenticationError) {
-    return <div>Error: You are not authenticated</div>
+    returnValue = <div>Error: You are not authenticated</div>
   } else if (error instanceof AuthorizationError) {
-    return (
+    returnValue = (
       <ErrorComponent
         statusCode={error.statusCode}
         title="Sorry, you are not authorized to access this"
       />
     )
   } else {
-    return (
+    returnValue = (
       <ErrorComponent
         statusCode={(error as any)?.statusCode || 400}
         title={error.message || error.name}
       />
     )
   }
+  return <ViewportCentered>{returnValue}</ViewportCentered>
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
