af67fc864a04f374bef6f80164b65871847fa774
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-05 03:30:29 +0200

    update autoinc sequences on seed
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,5 +1,6 @@
 // import db from "./index"
 
+import db from "db"
 import importPosts from "./importPosts"
 import { importUsers } from "./importUsers"
 
@@ -12,6 +13,19 @@ import { importUsers } from "./importUsers"
 const seed = async () => {
   await importUsers()
   await importPosts()
+
+  //update sequences because we inserted IDs explicitly
+
+  const updateSequences = [
+    `SELECT setval('"User_id_seq"', (SELECT MAX(id) FROM "User")+100)`,
+    `SELECT setval('"Post_id_seq"', (SELECT MAX(id) FROM "Post")+100)`,
+    `SELECT setval('"Image_id_seq"', (SELECT MAX(id) FROM "Image")+100)`,
+    `SELECT setval('"Category_id_seq"', (SELECT MAX(id) FROM "Category")+100)`,
+  ]
+  for (const q of updateSequences) {
+    const res = await db.$queryRawUnsafe(q)
+    console.log("UPDATE SEQUENCE", q, res)
+  }
 
   // for (let i = 0; i < 5; i++) {
   //   await db.project.create({ data: { name: "Project " + i } })
