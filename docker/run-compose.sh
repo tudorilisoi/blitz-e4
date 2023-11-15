a131c0adcd53dab4a749253435dab1daab3d8d6f
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-15 10:02:31 +0200

    cleanup
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- docker/run-compose.sh
+++ docker/run-compose.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export CURRENT_UID=$(id -u)
 export CURRENT_GUID=$(id -g)
+mkdir -p /tmp/e4-web-tmp
 # set +e
 # docker build --build-arg CURRENT_GUID=$CURRENT_GUID CURRENT_UID=$CURRENT_UID -f ./Dockerfile.nodejs
 docker-compose -f ./docker-compose.yml up --build
