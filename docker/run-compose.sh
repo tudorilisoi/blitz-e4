68e5bd90e6ea86c5c19aaa97793a46c9d652acc9
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-22 08:20:42 +0200

    detached run
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- docker/run-compose.sh
+++ docker/run-compose.sh
@@ -6,4 +6,5 @@ mkdir -p /tmp/e4-web-tmp
 mkdir -p /tmp/e4-web-cache
 # set +e
 # docker build --build-arg CURRENT_GUID=$CURRENT_GUID CURRENT_UID=$CURRENT_UID -f ./Dockerfile.nodejs
-docker-compose -f "$script_path/docker-compose.yml" up --build
+docker-compose -f "$script_path/docker-compose.yml" down 2>&1
+docker-compose -f "$script_path/docker-compose.yml" up --build -d
