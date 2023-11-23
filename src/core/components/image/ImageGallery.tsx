66d749a3dbae512234930103c6a7d84426af3913
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-23 15:35:10 +0200

    use hash to track slideshow open state
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/core/components/image/ImageGallery.tsx
+++ src/core/components/image/ImageGallery.tsx
@@ -1,10 +1,10 @@
 import Gallery from "react-photo-gallery"
 import { getImageUrl } from "./helpers"
-import Lightbox from "yet-another-react-lightbox"
+import Lightbox, { ControllerRef } from "yet-another-react-lightbox"
 import Zoom from "yet-another-react-lightbox/plugins/zoom"
 import Counter from "yet-another-react-lightbox/plugins/counter"
 import "yet-another-react-lightbox/styles.css"
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 const ImageGallery = ({ images }) => {
   const photos = images.map((i) => ({
     src: getImageUrl(i, true),
@@ -13,13 +13,46 @@ const ImageGallery = ({ images }) => {
   }))
   // return <Gallery photos={photos} />
 
-  const [index, setIndex] = useState(-1)
+  const [index, setIndex] = useState(0)
+  const [open, setOpen] = useState(false)
+  const ref = useRef<ControllerRef>(null)
   const openLightbox = useCallback((event, { photo, index }) => {
+    window.location.hash = `slideshow`
     setIndex(index)
+    setOpen(true)
   }, [])
   const closeLightbox = useCallback(() => {
-    setIndex(-1)
+    setOpen(false)
+    // window.location.hash = ``
+    // NOTE remove hash altogether
+    history.replaceState(null, document.title, location.pathname + location.search)
+  }, [ref])
+
+  useEffect(() => {
+    const currentHash = !!window.location.hash
+    console.log(`MOUNT:${currentHash}`)
+    setOpen(currentHash)
   }, [])
+
+  useEffect(() => {
+    const handleHashChange = (ev) => {
+      console.log(`🚀 ~ handleHashChange ~ window.location.hash:`, window.location.hash)
+      if (!window.location.hash) {
+        ref.current?.close()
+        setOpen(false)
+      } else {
+        setOpen(true)
+      }
+    }
+
+    // Attach the event listener when the component mounts
+    window.addEventListener("hashchange", handleHashChange)
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange)
+    }
+  }, [ref, open])
 
   const slides = images.map((i) => ({
     src: getImageUrl(i, false),
@@ -37,10 +70,11 @@ const ImageGallery = ({ images }) => {
 
       <Lightbox
         plugins={[Counter, Zoom]}
-        open={index > -1}
+        open={open}
         index={index}
         close={closeLightbox}
         slides={slides}
+        controller={{ ref }}
         counter={{ container: { style: { position: "absolute", top: "16px", left: "16px" } } }}
         zoom={{
           maxZoomPixelRatio: 3,
