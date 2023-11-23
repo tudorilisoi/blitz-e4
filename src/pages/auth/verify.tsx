b45842b0a3b0f6165243217d48ad73e3b924e76b
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-23 05:16:33 +0200

    WIP verify
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/pages/auth/verify.tsx
+++ src/pages/auth/verify.tsx
@@ -1,43 +1,30 @@
 import { BlitzPage } from "@blitzjs/next"
 import { useMutation } from "@blitzjs/rpc"
-import { useSearchParams } from "next/navigation"
-import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import verifyEmail from "src/auth/mutations/verifyEmail"
+import { gSSP } from "src/blitz-server"
 import { ErrorNotification } from "src/core/components/ErrorNotification"
 import { useOverlay } from "src/core/components/overlay/OverlayProvider"
 import Layout from "src/core/layouts/Layout"
 
-const VerifyPage: BlitzPage = () => {
-  const router = useRouter()
+export const getServerSideProps = gSSP(async (args) => {
+  const { query, ctx } = args
+  return { props: { query } }
+})
+
+const VerifyPage: BlitzPage<{ query }> = ({ query }) => {
+  const { activationKey, email } = query
   const { toggle, reset } = useOverlay()
-  const searchParams = useSearchParams()
-  // console.log(`🚀 ~ searchParams:`, searchParams)
-  const email = searchParams.get("email") || ""
-  const activationKey = searchParams.get("activationKey") || ""
   const [verifyMutation] = useMutation(verifyEmail)
-  const [loaded, setLoaded] = useState(0)
-  useEffect(() => {
-    setLoaded((v) => v + 1)
-  }, [])
+  const [verified, setVerified] = useState(false)
 
   useEffect(() => {
-    if (!loaded) {
-      return
-    }
-    if (!email || !activationKey) {
-      const error = new Error("Link de activare incomplet")
-      toggle(true, { component: <ErrorNotification error={error}></ErrorNotification> })
-      return
-    }
-
-    console.log(`render # ${loaded}`)
-    console.log(`🚀 ~ activationKey:`, loaded, activationKey, email)
-
     const activate = async (): Promise<void> => {
       try {
         toggle(true, reset)
         await verifyMutation({ email, activationKey })
+        setVerified(true)
+        toggle(false, { delay: 500 })
 
         // TODO toggle() a success component
       } catch (error) {
@@ -45,9 +32,16 @@ const VerifyPage: BlitzPage = () => {
       }
     }
     // NOTE fixes @typescript-eslint/no-floating-promises
-    activate().catch(() => {})
-  }, [searchParams, loaded])
-  return <></>
+    // activate().catch(() => {})
+    void activate()
+  }, [activationKey, email])
+
+  return (
+    <>
+      <h1>Se verifică</h1>
+      <pre>{JSON.stringify(query, null, 2)}</pre>
+    </>
+  )
 }
 VerifyPage.getLayout = (page) => <Layout title="Conectare">{page}</Layout>
 export default VerifyPage
