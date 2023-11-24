cbda9d7f6ba596b865c1702f5d2685a038ef6adb
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-24 10:57:50 +0200

    automate yarn install
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- docker/run-compose.sh
+++ docker/run-compose.sh
@@ -7,6 +7,7 @@ if grep -sq 'docker\|lxc' /proc/1/cgroup; then
     echo "Cannot run inside a container"
     exit 1;
 fi
+YARN="cd /app; rm -rf ./.data/uploads/miniaturi/*; rm -rf node_modules; yarn add husky blitz --dev --force --frozen-lockfile; yarn install;"
 #JEMALLOC="/usr/lib/x86_64-linux-gnu/libjemalloc.so.2"
 # NOTE running without WS
 if [ "$1" == "dev" ]; then
@@ -14,7 +15,7 @@ if [ "$1" == "dev" ]; then
     export WEB_CMD='yarn run dev:1-blitz'
 elif [ "$1" == "start" ]; then
     echo "Running start command"
-    export WEB_CMD='yarn build:1-blitz; yarn run start:1-blitz'
+    export WEB_CMD="$YARN yarn build:1-blitz; yarn run start:1-blitz"
 elif [ "$1" == "stop" ]; then
     echo "Running stop command"
     docker-compose -f "$script_path/docker-compose.yml" down 2>&1
