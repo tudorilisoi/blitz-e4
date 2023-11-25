a8672c886dc2311cf9710f872a2fc2860ee9431a
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-25 11:06:27 +0200

    add greater mem limit for dev
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- docker/run-compose.sh
+++ docker/run-compose.sh
@@ -12,10 +12,12 @@ yarn add husky blitz --dev --force --frozen-lockfile; yarn install;\
 yarn build:1-blitz;
 "
 EXTRA_ARGS="-d"
+export WEB_MEM_LIMIT="1G"
 #JEMALLOC="/usr/lib/x86_64-linux-gnu/libjemalloc.so.2"
 # NOTE running without WS
 if [ "$1" == "dev" ]; then
     echo "Running development command"
+    export WEB_MEM_LIMIT="4G"
     export WEB_CMD='yarn run dev:1-blitz'
 elif [ "$1" == "start" ]; then
     echo "Running start command"
