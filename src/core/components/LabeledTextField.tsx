f3516b88b16de497945c2ad36371c4d87af797ba
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-25 17:43:31 +0200

    add eye to password field
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/core/components/LabeledTextField.tsx
+++ src/core/components/LabeledTextField.tsx
@@ -1,6 +1,7 @@
-import { forwardRef, PropsWithoutRef, ComponentPropsWithoutRef, Ref } from "react"
+import { forwardRef, PropsWithoutRef, ComponentPropsWithoutRef, Ref, useState } from "react"
 import { useFormContext } from "react-hook-form"
 import { ErrorMessage } from "@hookform/error-message"
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline"
 
 export const FieldDefaultAsTypeDefault = "input" as const
 export type FieldDefaultAsType = typeof FieldDefaultAsTypeDefault
@@ -15,6 +16,38 @@ export type FieldOwnProps<E extends React.ElementType> = {
   // type?: "text" | "password" | "email" | "number"
   outerProps?: PropsWithoutRef<JSX.IntrinsicElements["div"]>
   labelProps?: ComponentPropsWithoutRef<"label">
+}
+
+export const PasswordField = (props) => {
+  console.log(`🚀 ~ PasswordField ~ props:`, props)
+  const [visible, setvisible] = useState(false)
+
+  const { Tag, tagProps } = props
+  let cn = tagProps.className || ""
+  cn = `${cn} join-item`
+  cn = `input join-item bordered border-r-0`
+  const newProps = {
+    ...tagProps,
+    type: visible ? "text" : "password",
+    className: cn,
+  }
+
+  const input = <Tag {...newProps} />
+  const Icon = visible ? EyeSlashIcon : EyeIcon
+
+  return (
+    <div className="join w-fit group bordered focus-within:border-2">
+      {input}
+      <div
+        className=" btn btn-ghost join-item border-l-2  bordered"
+        onClick={() => setvisible((prevValue) => !prevValue)}
+      >
+        <span>
+          <Icon className="inline-block h-6" />
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export type FieldProps<E extends React.ElementType> = FieldOwnProps<E> &
@@ -38,16 +71,25 @@ export const LabeledTextFieldInner = <E extends React.ElementType = FieldDefault
 
   // console.log("Form errors", errors)
   const labelId = `label_${name}`
+  const tprops = {
+    id: labelId,
+    disabled: isSubmitting,
+    ...register(name),
+    ...otherProps,
+  }
+  let tag
 
+  if (otherProps.type === "password") {
+    tag = <PasswordField Tag={Tag} tagProps={tprops} />
+  } else {
+    tag = <Tag {...tprops}>{children}</Tag>
+  }
   return (
     <div {...outerProps}>
       <label {...labelProps} htmlFor={labelId}>
         {label}
       </label>
-      <Tag id={labelId} disabled={isSubmitting} {...register(name)} {...otherProps}>
-        {children}
-      </Tag>
-
+      {tag}
       <ErrorMessage
         render={({ message }) => (
           <span
