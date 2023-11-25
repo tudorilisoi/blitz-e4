ab1dce2a81b863e5ba7b622ca80c13f761b5c420
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-25 08:44:52 +0200

    add service worker removal
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 // @ts-check
 const { withBlitz } = require("@blitzjs/next")
+const RemoveServiceWorkerPlugin = require("webpack-remove-serviceworker-plugin")
 
 /**
  * @type {import('@blitzjs/next').BlitzConfig}
  **/
 const config = {
+  webpack: (config) => {
+    plugins: [new RemoveServiceWorkerPlugin()]
+    return config
+  },
   experimental: {
     optimizeCss: true,
   },
