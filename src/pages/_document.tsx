bd37397dd51e93a2cb99209b289ed9d5bee25e55
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-26 16:04:29 +0200

    fix build
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,52 @@
-import Document, { Html, Main, NextScript, Head } from "next/document"
-import DocumentHead from "src/core/components/DocumentHead"
+import Document, { Head, Html, Main, NextScript } from "next/document"
+
+function dedupeHead(elems) {
+  const result: React.ReactElement[] = []
+  const buffer: Record<string, React.ReactElement> = {}
+
+  if (elems) {
+    for (let i = 0, len = elems.length; i < len; i++) {
+      const elem = elems[i],
+        type = elem.type,
+        props = elem.props
+      let uniqueProp = props.itemprop || props.property || props.name || props.href
+      let key = `${type}::uniqueProp: ${uniqueProp}`
+
+      /** Dedupe */
+      switch (type) {
+        case "meta":
+        case "title":
+          if (uniqueProp === "article:tag") {
+            console.log(`ACC/SKIP ${key}`, key, elem)
+            result.push(elem)
+            break
+          }
+          if (buffer[key] !== undefined) {
+            // console.log("ADD meta", key, elem)
+            console.log(`OWR [${key}]`, buffer[key]?.props, elem.props)
+          }
+          buffer[key] = elem
+          break
+        default:
+          result.push(elem)
+          console.log(`ADD/SKIP [${key}]`, elem.props)
+          break
+      }
+    }
+    Object.values(buffer).forEach((element) => {
+      result.push(element)
+    })
+  }
+
+  return result
+}
+
+class DocumentHead extends Head {
+  render() {
+    this.context.head = dedupeHead(this.context.head || [])
+    return super.render()
+  }
+}
 
 class MyDocument extends Document {
   // Only uncomment if you need to customize this behaviour
