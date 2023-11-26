373c69b8fa808444a1217fcf511161234c5e97c9
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-11-26 13:57:21 +0200

    more sync
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- docker/sync.sh
+++ docker/sync.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# Get the current hostname
+current_hostname=$(hostname)
+
+if grep -sq 'docker\|lxc' /proc/1/cgroup; then
+    echo "Cannot run inside a container"
+    exit 1;
+fi
+
+# Check if the hostname is "mintbox"
+if [ "$current_hostname" = "mintbox" ]; then
+  echo "Exiting script because the hostname is mintbox."
+  exit 1
+else
+  echo "Continuing script execution with hostname: $current_hostname"
+fi
+
 script_path=$(dirname "$(readlink -f "$0")")
 
 echo "git reset --hard"
