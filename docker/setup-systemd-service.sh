e6142782d4edf914ff1879f44b5424c00b0a8c28
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-12-04 14:40:56 +0200

    cleanup
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- docker/setup-systemd-service.sh
+++ docker/setup-systemd-service.sh
@@ -7,10 +7,37 @@ gpg2 --gen-key
 # create the password store using the gpg user id
 pass init $gpg_id
 
+
+
+if [ $# -eq 0 ]; then
+    echo  "Usage: $0 folderName [-u]"
+    echo  "-u: uninstall"
+    exit 1;
+fi
 # TODO make a copy and substitute workdir and $1
 
-sudo cp docker-compose@.service /etc/systemd/system/docker-compose@pauls.ro.service
-sudo systemctl daemon-reload
-sudo systemctl restart docker-compose@pauls.ro
+clear
 
-journalctl -f -u docker-compose@pauls.ro
+BASE_DIR="/home/tudor/Documents/dev"
+# production @ionos
+# BASE_DIR="/home/tudor/www"
+
+SRV_NAME=$1
+SERVICE_FILE="docker-compose@$SRV_NAME.service"
+# cp docker-compose@.service "$SERVICE_FILE"
+WDIR="$BASE_DIR/$SRV_NAME/docker"
+
+rm "/tmp/$SERVICE_FILE" 2>&1
+sed "s#WDIR#$WDIR#g" docker-compose@.service > "/tmp/$SERVICE_FILE"
+
+cat "/tmp/$SERVICE_FILE"
+
+#debug
+# exit
+
+sudo cp "/tmp/$SERVICE_FILE" "/etc/systemd/system/$SERVICE_FILE"
+rm "/tmp/$SERVICE_FILE"
+sudo systemctl daemon-reload
+sudo systemctl restart "$SERVICE_FILE"
+
+journalctl -f -u "$SERVICE_FILE"
