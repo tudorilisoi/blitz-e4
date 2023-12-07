f5b509f203152089b01533c1a3a6a51577d0c2ff
Tudor Ilisoi <tudorilisoi@yahoo.com>
2023-12-07 13:39:08 +0200

    GTag
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/core/components/gtag.tsx
+++ src/core/components/gtag.tsx
@@ -6,24 +6,27 @@ const isProduction = true
 export const GTag = () => {
   const router = useRouter()
 
+  const pushGTag = () => {
+    const page = {
+      page_location: window.location.href,
+      page_path: window.location.pathname,
+      page_title: document.title,
+      send_to: process.env.NEXT_PUBLIC_GTAG_ID,
+    }
+    console.log("GTag push", page.page_title, page.page_location)
+    // @ts-ignore
+    window.dataLayer.push({
+      event: "page_view",
+      page,
+    })
+  }
+
+  useEffect(pushGTag, [])
   useEffect(() => {
     const handleRouteChange = (url: URL) => {
       /* invoke analytics function only for production */
       if (isProduction) {
-        window.setTimeout(() => {
-          const page = {
-            page_location: window.location.href,
-            page_path: window.location.pathname,
-            page_title: document.title,
-            send_to: process.env.NEXT_PUBLIC_GTAG_ID,
-          }
-          console.log("GTag push", page.page_title, page.page_location)
-          // @ts-ignore
-          window.dataLayer.push({
-            event: "page_view",
-            page,
-          })
-        }, 100)
+        window.setTimeout(pushGTag, 100)
       }
     }
     router.events.on("routeChangeComplete", handleRouteChange)
