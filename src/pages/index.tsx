1bd6aa2f1b34337f5fe570926e4246bb0c9ec6d4
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-02-29 11:03:22 +0200

    Tavidor promo
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,10 +28,20 @@ function AboutPage() {
     </div>
   )
 }
-
+const AD_SRC =
+  "https://www.e-suceava.ro/wp-content/uploads/2024/01/Tavidor-firma-recomandata-in-Tamplarie-PVC-si-Aluminiu-de-calitatea-superioara.jpg"
+const AD_HREF = "https://www.tavidor.ro/tamplarie-pvc/"
+const AD_TITLE = "Tavidor, firmă recomandată în Tâmplărie PVC de calitate superioară"
 const Home = ({ categories }) => {
   return (
     <>
+      <a
+        href={AD_HREF}
+        className="block m-auto text-center text-accent  bg-neutral-700 rounded-md py-4 mb-6"
+      >
+        <p className="relative inset-0 flex-col mb-2">PROMO: {AD_TITLE}</p>
+        <img src={AD_SRC} alt={AD_TITLE} className="m-auto max-h-[20vh] rounded-lg" />
+      </a>
       <div className="prose mb-3">
         <h1 className="text-2xl text-base-content">Anunţuri</h1>
       </div>
