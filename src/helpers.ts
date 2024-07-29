c29f2240c068c289446437e7ba72986db51feb8d
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-07-30 01:55:16 +0300

    fix es5 target
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/helpers.ts
+++ src/helpers.ts
@@ -40,7 +40,7 @@ export const shortenText = (str, maxLength, omission = "...") => {
 }
 
 export const nl2space = (str) => {
-  const RE = /[\r|\n|\s|\W|\t]+/gmu
+  const RE = /[\r|\n|\s|\W|\t]+/gm
   const ret = str.replace(RE, " ")
   return ret
 }
@@ -48,7 +48,7 @@ export const nl2space = (str) => {
 export const nl2br = (str) => {
   // console.log(str)
   return str
-    .split(/(\r|\n)+/mu)
+    .split(/(\r|\n)+/m)
     .filter((i) => {
       // console.log(`[${i}]`)
       return i.trim() !== ""
@@ -65,7 +65,7 @@ export const nl2br = (str) => {
 }
 
 export const obscurePhoneNumbers = (str) => {
-  let re = /([+](\s+)?)?\d(\d|\s|[.-/]){8,}\d/gmu
+  let re = /([+](\s+)?)?\d(\d|\s|[.-/]){8,}\d/gm
   return str.replace(re, "********")
 }
 
