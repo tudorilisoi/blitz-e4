2cea5b1bf5496a718c82a77f79687851508c2cb0
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-07-31 20:44:07 +0300

    Revert "WIP clerk"
    This reverts commit c7286af5c9bbba6b83feed870578158725d3846d.

--- src/blitz-server.ts
+++ src/blitz-server.ts
@@ -1,31 +1,9 @@
-import { AuthServerPlugin, BlitzCtx, PrismaStorage, simpleRolesIsAuthorized } from "@blitzjs/auth"
 import { setupBlitzServer } from "@blitzjs/next"
-import { BlitzLogger, createServerPlugin, RequestMiddleware } from "blitz"
+import { AuthServerPlugin, PrismaStorage } from "@blitzjs/auth"
+import { simpleRolesIsAuthorized } from "@blitzjs/auth"
+import { BlitzLogger } from "blitz"
 import db from "db"
-import { IncomingMessage, ServerResponse } from "http"
 import { authConfig } from "./blitz-client"
-import { clerkOptions } from "./clerkOptions"
-import { clerkClient } from "@clerk/nextjs/server"
-
-const cplugin = createServerPlugin((options) => {
-  const mw: RequestMiddleware<
-    IncomingMessage & { clerkAuth: any },
-    ServerResponse & { blitzCtx: BlitzCtx }
-  > = async (req, res, next) => {
-    console.log("Fuyoo", req.url, req.headers["x-clerk-auth"])
-    return next()
-  }
-
-  mw.config = {
-    name: "mw",
-  }
-
-  const { publishableKey, secretKey } = clerkOptions
-  return {
-    requestMiddlewares: [mw],
-    exports: () => ({}),
-  }
-})
 
 export const authPlugin = AuthServerPlugin({
   ...authConfig,
@@ -34,7 +12,7 @@ export const authPlugin = AuthServerPlugin({
 })
 
 export const { gSSP, gSP, api } = setupBlitzServer({
-  plugins: [authPlugin, cplugin({})],
+  plugins: [authPlugin],
   // NOTE tslog comes with default log level 0: silly, 1: trace, 2: debug, 3: info, 4: warn, 5: error, 6: fatal.
   logger: BlitzLogger({
     minLevel: 3,
