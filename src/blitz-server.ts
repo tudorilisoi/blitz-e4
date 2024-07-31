931afa791fcc3126fe81a0c03a49dfdd9885fa8d
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-07-31 20:44:04 +0300

    Revert "WIP clerk"
    This reverts commit b0a1c8a57da2086f33530b6f28f6cebc65e21c4a.

--- src/blitz-server.ts
+++ src/blitz-server.ts
@@ -1,12 +1,13 @@
 import { AuthServerPlugin, BlitzCtx, PrismaStorage, simpleRolesIsAuthorized } from "@blitzjs/auth"
 import { setupBlitzServer } from "@blitzjs/next"
-import { ClerkMiddlewareAuthObject } from "@clerk/nextjs/dist/types/server"
 import { BlitzLogger, createServerPlugin, RequestMiddleware } from "blitz"
 import db, { UserRoles } from "db"
 import { IncomingMessage, ServerResponse } from "http"
+import { authConfig } from "./blitz-client"
+import { ClerkMiddlewareAuthObject } from "@clerk/nextjs/dist/types/server"
 import { nanoid } from "nanoid"
 import { Role } from "types"
-import { authConfig } from "./blitz-client"
+import { isAuthenticated } from "./auth/helpers"
 
 export async function handleClerkAuth(authObj: ClerkMiddlewareAuthObject, ctx: BlitzCtx) {
   if (authObj.sessionClaims) {
@@ -25,12 +26,14 @@ export async function handleClerkAuth(authObj: ClerkMiddlewareAuthObject, ctx: B
         select: userSelect,
       })
     }
-    console.log(`🚀 ~ handleClerkAuth ~ ctx.session 1:`, ctx.session.userId)
-    if (!ctx.session.userId) {
-      //login the user
-      console.log(`🚀 ~ handleClerkAuth ~ login:`, user)
-      await ctx.session.$create({ userId: user.id, role: user.role as Role })
-      console.log(`🚀 ~ handleClerkAuth ~ ctx.session 2:`, ctx.session.userId)
+    if (user) {
+      console.log(`🚀 ~ handleClerkAuth ~ ctx.session 1:`, ctx.session.userId)
+      if (!ctx.session.userId) {
+        //login the user
+        console.log(`🚀 ~ handleClerkAuth ~ login:`, user)
+        await ctx.session.$create({ userId: user.id, role: user.role as Role })
+        console.log(`🚀 ~ handleClerkAuth ~ ctx.session 2:`, ctx.session.userId)
+      }
     }
   }
 }
