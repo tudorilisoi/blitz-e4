e34349d38ad552fdab210d51eecaa8394b8d2351
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-10-05 12:01:45 +0300

    add SR fade in and debounce
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/pages/cautare/[[...params]].tsx
+++ src/pages/cautare/[[...params]].tsx
@@ -8,8 +8,18 @@ import { searchClient } from "src/meili/client"
 import PostCell from "src/posts/components/PostCell/PostCell"
 import { PostWithIncludes } from "src/posts/helpers"
 import { useEffect, useRef } from "react"
+import styles from "./search.module.css"
 
 const DEFAULT_SORT = "Post:updatedTimestamp:desc"
+
+let timerId
+function queryHook(query, search) {
+  if (timerId) {
+    clearTimeout(timerId)
+  }
+
+  timerId = setTimeout(() => search(query), 200)
+}
 
 export default function SearchPage({}) {
   // NOTE docs here https://www.algolia.com/doc/api-reference/widgets/infinite-hits/react/
@@ -45,6 +55,7 @@ export default function SearchPage({}) {
         />
         <div className="">
           <SearchBox
+            queryHook={queryHook}
             classNames={{
               input: "w-full p-4 mb-4",
               form: "",
@@ -59,16 +70,20 @@ export default function SearchPage({}) {
 }
 SearchPage.getLayout = (page) => <Layout>{page}</Layout>
 
-const Hit = ({ hit }) => {
+const Hit = ({ hit, index }) => {
   // return <pre>{JSON.stringify(props, null, 2)}</pre>
   // console.log(hit.updatedTimestamp)
   const post = hit as PostWithIncludes
   return (
-    <div className="mb-2">
+    <div className="mb-2" style={{ opacity: 0, animation: animStr(index) }}>
       <PostCell post={post} />
     </div>
   )
 }
+
+const duration = 200 // ms
+const delay = 100 // ms
+const animStr = (i) => `${styles.fadeIn} ${duration}ms ease-out ${delay * i}ms forwards`
 
 // NOTE see https://www.algolia.com/doc/guides/building-search-ui/ui-and-ux-patterns/infinite-scroll/react/
 function CustomInfiniteHits(props) {
@@ -108,7 +123,9 @@ function CustomInfiniteHits(props) {
   return (
     <>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
-        {isInitial ? null : items.map((item) => <Hit key={item.id} hit={item} />)}
+        {isInitial
+          ? null
+          : items.map((item, index) => <Hit key={item.id} hit={item} index={index % 3} />)}
       </div>
       {/* <div ref={sentinelRef} aria-hidden="true" /> */}
       {isInitial ? null : (
