7eb602fb51bd3fbca4b2981330177ca7cb470624
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-10-28 21:00:45 +0200

    latest posts API
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -8,19 +8,17 @@ import { makePostNavUrl } from "src/pages/anunt/[[...params]]"
 import { makePostsNavUrl } from "src/pages/anunturi/[[...params]]"
 const LIMIT = 20
 async function getAPIPosts(req, res: ServerResponse) {
-  console.log(req.query)
-  let mappedPosts: unknown = []
-  let { categoryId } = req.query
-
-  categoryId = parseInt(categoryId, 10)
-  if (!isNaN(categoryId)) {
+  const categories = await db.category.findMany({ orderBy: { title: "asc" } })
+  let mappedCategories: any[] = []
+  for (let category of categories) {
     const posts = await db.post.findMany({
-      where: { categoryId, status: { not: PostStatuses.EXPIRED } },
+      where: { categoryId: category.id, status: { not: PostStatuses.EXPIRED } },
       orderBy: { updatedAt: "desc" },
       take: LIMIT,
       include: postInclude,
     })
-    mappedPosts = posts.map((post) => {
+
+    const mappedPosts = posts.map((post) => {
       return {
         title: post.title,
         body: post.body,
@@ -31,22 +29,19 @@ async function getAPIPosts(req, res: ServerResponse) {
         images: post.images.map((image) => canonical(getImageUrl(image, true))),
       }
     })
-  }
-  const categories = await db.category.findMany({ orderBy: { title: "asc" } })
-  const mappedCategories = categories.map((category) => {
-    return {
+
+    mappedCategories.push({
       id: category.id,
       title: category.title,
       url: canonical(makePostsNavUrl(category.slug)),
-      apiUrl: canonical(`/api/posts?categoryId=${category.id}`),
-    }
-  })
+      posts: mappedPosts,
+    })
+  }
   res.setHeader("Content-Type", "application/json")
   res.setHeader("Cache-Control", `public, max-age=${3600}, stale-while-revalidate=59`)
   res.end(
     JSON.stringify(
       {
-        posts: mappedPosts,
         categories: mappedCategories,
       },
       null,
@@ -55,7 +50,7 @@ async function getAPIPosts(req, res: ServerResponse) {
   )
 }
 
-export default async (req, res) => {
+const postsAPI = async (req, res) => {
   try {
     await getAPIPosts(req, res)
   } catch (error) {
@@ -63,3 +58,5 @@ export default async (req, res) => {
     res.end("Internal Server Error")
   }
 }
+
+export default postsAPI
