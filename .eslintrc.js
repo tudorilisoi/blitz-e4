e14a28f9200b891a3d9c40c6a13363f02276066f
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-12-12 08:13:02 +0200

    customize .eslintrc
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,6 @@
-module.exports = require("@blitzjs/next/eslint")
+const config = require("@blitzjs/next/eslint")
+const extendedConfig = {
+  ...config,
+  rules: { ...config.rules, "@next/next/no-img-element": "off" },
+}
+module.exports = extendedConfig
