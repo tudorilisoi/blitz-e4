6fd3d2be471d0b854debccf2f591186fffcb6d78
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-03-24 15:41:37 +0200

    cleanup
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/core/components/spinner/ViewPortCentered.tsx
+++ src/core/components/spinner/ViewPortCentered.tsx
@@ -1,4 +1,5 @@
-const ViewportCentered = ({ children }) => {
+import React from "react"
+const ViewportCentered = ({ children }): JSX.Element => {
   return (
     <div className="z-[100]  pointer-events-none top-0 left-0 right-0 bottom-0 fixed flex justify-center flex-col align-middle text-center ">
       <div className="pointer-events-auto">{children}</div>
