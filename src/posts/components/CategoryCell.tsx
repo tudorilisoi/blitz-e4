2f1c97464c8533d682931cd224070dd55f1e8a44
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-03-25 20:34:13 +0200

    add pointer cursor to category cell
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/posts/components/CategoryCell.tsx
+++ src/posts/components/CategoryCell.tsx
@@ -2,15 +2,19 @@ import Link from "next/link"
 import { pluralize, shortenText } from "src/helpers"
 import { makePostsNavUrl } from "src/pages/anunturi/[[...params]]"
 import { CategoryWithCounters } from "../helpers"
+import { useRouter } from "next/router"
 const CategoryCell = ({ category }: { category: CategoryWithCounters }) => {
   const { postCount } = category
+  const router = useRouter()
+  const url = makePostsNavUrl(category.slug, 1)
   return (
     <section
       key={category.id}
-      className="p-4 rounded-md  bg-primary bg-opacity-20  text-base-content "
+      className="p-4 rounded-md  bg-primary bg-opacity-20  text-base-content cursor-pointer "
+      onClick={() => router.push(url)}
     >
       <h2 className="text-xl font-semibold mb-2 text-accent hover:text-accent-focus hover:underline">
-        <Link rel="prefetch" href={makePostsNavUrl(category.slug, 1)}>
+        <Link rel="prefetch" href={url}>
           {shortenText(category.title, 100)}
         </Link>
       </h2>
