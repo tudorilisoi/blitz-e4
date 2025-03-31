63b2b3dcb3f64460a92cba5d2316e16f1b2cf04a
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-03-31 08:01:59 +0300

    fix layout
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/posts/components/CategoryCell.tsx
+++ src/posts/components/CategoryCell.tsx
@@ -19,7 +19,7 @@ const CategoryCell = ({ category }: { category: CategoryWithCounters }) => {
           <span className="mr-2 border  p-0 rounded-badge  h-[2.5rem] w-[2.5rem] inline-flex place-content-center place-items-center">
             <CategoryIcon categoryId={category.id} className="text-white  " />
           </span>
-          {shortenText(category.title, 100)}
+          <span className="break-words">{shortenText(category.title, 100)}</span>
         </Link>
       </h2>
 
