9b167ceb228910e841ea5a4950bfd8c23d1beacf
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-04-26 09:47:16 +0300

    set query to undefined when empty
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/pages/cautare/[[...params]].tsx
+++ src/pages/cautare/[[...params]].tsx
@@ -37,7 +37,7 @@ function queryHook(query, search) {
     clearTimeout(timerId)
   }
 
-  timerId = setTimeout(() => search(query), 500)
+  timerId = setTimeout(() => search(query || undefined), 500)
 }
 
 const isSearchPageActive = () => window.location.pathname === "/cautare"
