7ce8cc343af7d259d8e3613a2a44a05f1cc8d161
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-05-07 17:36:37 +0300

    WIP promo badge
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/posts/components/PostCell/PostCell.tsx
+++ src/posts/components/PostCell/PostCell.tsx
@@ -17,13 +17,14 @@ const PostCell = ({ post }: { post: PostWithIncludes }) => {
   const imgStyle = {
     backgroundImage: `url("${imageUrl}")`,
   }
+  const isPromoted = post.promotionLevel > 0
 
   return (
     <section
       onClick={() => router.push(url)}
       className={`${styles.postCell} relative min-w-0 h-full group border border-neutral hover:border-primary  flex flex-row flex-nowrap p-0 rounded-md rounded-l-2xl
 max-w-[100%]
-      bg-primary bg-opacity-20 shadow-sm cursor-pointer`}
+      bg-primary bg-opacity-20 shadow-sm cursor-pointer overflow-hidden`}
     >
       <div style={imgStyle} className={`${styles.bgImage} flex-none w-[30%] rounded-l-2xl`}></div>
       {/*  <div className="absolute top-0 right-0 w-0 h-0 border-t-[60px] border-l-[60px] border-t-red-600 border-l-transparent">
@@ -31,7 +32,7 @@ max-w-[100%]
           PROMO
         </div>
       </div> */}
-      {post.promotionLevel === 0 ? null : (
+      {!isPromoted ? null : (
         <div className="absolute top-0 left-0 w-0 h-0 border-t-[60px] border-r-[60px] border-t-red-600 border-r-transparent">
           <div className="absolute -top-10 left-0 w-auto text-white text-[10px] font-bold transform -rotate-45  text-center pointer-events-none">
             PROMO
@@ -39,7 +40,9 @@ max-w-[100%]
         </div>
       )}
       <div className="flex-none w-[70%] max-w-[70%] ">
-        <div className="bg-primary bg-opacity-40 group-hover:bg-opacity-80 rounded-tr-md  ">
+        <div
+          className={`${isPromoted ? "bg-secondary" : "bg-primary"} bg-opacity-40 group-hover:bg-opacity-80 rounded-tr-md  `}
+        >
           <div className="p-2 pl-3">
             <Link href={url}>
               <h2 className="text-lg  font-semibold text-primary-content  break-words line-clamp-2 min-h-[3.2rem] ">
