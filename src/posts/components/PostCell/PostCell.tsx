3d5be045103e3562177a04ece0b8839af527d65f
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-05-07 17:31:58 +0300

    WIP promo badge
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/posts/components/PostCell/PostCell.tsx
+++ src/posts/components/PostCell/PostCell.tsx
@@ -21,11 +21,23 @@ const PostCell = ({ post }: { post: PostWithIncludes }) => {
   return (
     <section
       onClick={() => router.push(url)}
-      className={`${styles.postCell} min-w-0 h-full group border border-neutral hover:border-primary  flex flex-row flex-nowrap p-0 rounded-md rounded-l-2xl
+      className={`${styles.postCell} relative min-w-0 h-full group border border-neutral hover:border-primary  flex flex-row flex-nowrap p-0 rounded-md rounded-l-2xl
 max-w-[100%]
       bg-primary bg-opacity-20 shadow-sm cursor-pointer`}
     >
       <div style={imgStyle} className={`${styles.bgImage} flex-none w-[30%] rounded-l-2xl`}></div>
+      {/*  <div className="absolute top-0 right-0 w-0 h-0 border-t-[60px] border-l-[60px] border-t-red-600 border-l-transparent">
+        <div className="absolute -top-10 right-0 w-auto text-white text-[10px] font-bold transform rotate-45  text-center pointer-events-none">
+          PROMO
+        </div>
+      </div> */}
+      {post.promotionLevel === 0 ? null : (
+        <div className="absolute top-0 left-0 w-0 h-0 border-t-[60px] border-r-[60px] border-t-red-600 border-r-transparent">
+          <div className="absolute -top-10 left-0 w-auto text-white text-[10px] font-bold transform -rotate-45  text-center pointer-events-none">
+            PROMO
+          </div>
+        </div>
+      )}
       <div className="flex-none w-[70%] max-w-[70%] ">
         <div className="bg-primary bg-opacity-40 group-hover:bg-opacity-80 rounded-tr-md  ">
           <div className="p-2 pl-3">
